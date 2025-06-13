0d04401f20cc3b024fd57b51deac22a2e61be4db
CodeByMohana <137144689+CodeByMohana@users.noreply.github.com>
2025-06-13 09:40:48 +0530

    [Tunuguntla Mohana Sai].Add Practice Problems For Day 1

---
+++ PracticeProblems/AreaOfCircle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class AreaOfCircle {
+    static public void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter radius of circle : ");
+        float radius = sc.nextFloat();
+
+        float Area = (3.14F) * radius * radius;
+        System.out.println("Area of circle : " +Area+"sq");
+    }
+}
