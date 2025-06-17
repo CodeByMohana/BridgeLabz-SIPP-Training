3febac3034ebe2c8391893ca5d4c0348f4ae8007
CodeByMohana <137144689+CodeByMohana@users.noreply.github.com>
2025-06-17 11:08:07 +0530

    [Tunuguntla Mohana Sai].Add Arrays for Day4

---
+++ Arrays/question_8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class question_8  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] heights = new double[11];
+        double sum = 0;
+
+        System.out.println("Enter the heights of 11 football players (in cm):");
+        for (int i = 0; i < heights.length; i++) {
+            System.out.print("Height of player " + (i + 1) + ": ");
+            heights[i] = sc.nextDouble();
+            sum += heights[i];
+        }
+
+        double mean = sum / 11;
+
+        System.out.print("The mean height of the football players is: " + mean + "cm");
+
+        sc.close();
+    }
+}
